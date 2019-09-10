c6432e99ca68461b9d0e529b3672e079d7aa72d7
Liam Sutton <devliam90@gmail.com>
2019-09-10 15:11:17 +0100

    add all imports and exports to sub components

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,8 +1,10 @@
 import React from "react";
 
 //import any components needed
+import NumberButton from './NumberButton';
 
 //Import your array data to from the provided data file
+import { numbers } from '../../../data';
 
 const Numbers = () => {
   // STEP 2 - add the imported data to state
@@ -14,3 +16,5 @@ const Numbers = () => {
     </div>
   );
 };
+
+export default Numbers
